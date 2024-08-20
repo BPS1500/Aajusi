b34b822fedf652494ab61437cbf3154124e154eb
devadanendra <devadanendra21@gmail.com>
2024-08-20 13:59:12 +0700

    update landing page

--- app/Views/layouts/landing_layout.php
+++ app/Views/layouts/landing_layout.php
@@ -42,16 +42,16 @@
             </a>
 
             <nav id="navmenu" class="navmenu">
-                <ul>
+                <!-- <ul>
                     <li><a href="#hero" class="active">Beranda</a></li>
                     <li><a href="#about">Tentang</a></li>
                     <li><a href="#services">Layanan</a></li>
                     <li><a href="#contact">Hubungi</a></li>
-                </ul>
+                </ul> -->
                 <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
             </nav>
 
-            <div class="header-social-links">
+            <!-- <div class="header-social-links">
                 <a href="https://jambi.bps.go.id/" class="twitter" target="_blank" rel="noopener noreferrer">
                     <i class="bi bi-twitter-x"></i>
                 </a>
@@ -64,16 +64,16 @@
                 <a href="https://www.youtube.com/channel/UCIg5Vi_IlqQ88EWCJ42fh8Q" class="youtube" target="_blank" rel="noopener noreferrer">
                     <i class="bi bi-youtube"></i>
                 </a>
-            </div>
+            </div> -->
 
         </div>
     </header>
 
     <main class="main">
         <!-- Hero Section -->
-        <section id="hero" class="hero section">
+        <section id="hero" class="hero section"  style="margin-top: -100px;">
             <div class="container d-flex flex-column justify-content-center align-items-center text-center position-relative" data-aos="zoom-out">
-                <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+                <img src="assets/Bikin/assets/img/hero-img.png" class="img-fluid animated" alt="">
                 <h1>Selamat datang di <span>AAJUSI</span></h1>
                 <p>Aplikasi Pengajuan Publikasi BPS Provinsi Jambi</p>
                 <div class="d-flex">
@@ -82,123 +82,6 @@
             </div>
         </section>
         <!-- /Hero Section -->
-
-        <!-- About Section -->
-        <section id="about" class="about section">
-            <!-- Section Title -->
-            <div class="container section-title" data-aos="fade-up">
-                <h2>Tentang AAJUSI</h2>
-                <p>Sistem yang mempermudah proses pengajuan dan manajemen publikasi statistik secara online oleh BPS Provinsi Jambi</p>
-            </div>
-            <!-- End Section Title -->
-            <div class="container">
-                <div class="row gy-4">
-                    <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                            magna aliqua.
-                        </p>
-                        <ul>
-                            <li><i class="bi bi-check2-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
-                            <li><i class="bi bi-check2-circle"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
-                            <li><i class="bi bi-check2-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo</span></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
-                        <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- /About Section -->
-
-        <!-- Services Section -->
-        <section id="services" class="services section">
-            <!-- Section Title -->
-            <div class="container section-title" data-aos="fade-up">
-                <h2>Layanan</h2>
-                <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
-            </div>
-            <!-- End Section Title -->
-            <div class="container">
-                <div class="row gy-4">
-                    <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-                        <div class="icon">
-                        <i class="bi bi-activity"></i>
-                        </div>
-                        <a href="service-details.html" class="stretched-link">
-                        <h3>Nesciunt Mete</h3>
-                        </a>
-                        <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
-                    <div class="service-item position-relative">
-                        <div class="icon">
-                            <i class="bi bi-broadcast"></i>
-                        </div>
-                        <a href="service-details.html" class="stretched-link">
-                            <h3>Eosle Commodi</h3>
-                        </a>
-                        <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
-                    <div class="service-item position-relative">
-                        <div class="icon">
-                            <i class="bi bi-easel"></i>
-                        </div>
-                        <a href="service-details.html" class="stretched-link">
-                            <h3>Ledo Markt</h3>
-                        </a>
-                        <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- /Services Section -->
-
-        <!-- Contact Section -->
-        <section id="contact" class="contact section light-background">
-            <!-- Section Title -->
-            <div class="container section-title" data-aos="fade-up">
-                <h2>Hubungi</h2>
-                <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
-            </div>
-            <!-- End Section Title -->
-
-            <div class="container" data-aos="fade" data-aos-delay="100">
-                <div class="row gy-4">
-                    <div class="col-lg-4">
-                        <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="200">
-                            <i class="bi bi-geo-alt flex-shrink-0"></i>
-                        <div>
-                        <h3>Address</h3>
-                        <p>A108 Adam Street, New York, NY 535022</p>
-                    </div>
-                </div>
-
-                <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
-                    <i class="bi bi-telephone flex-shrink-0"></i>
-                    <div>
-                        <h3>Call Us</h3>
-                        <p>+1 5589 55488 55</p>
-                    </div>
-                </div>
-
-                <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
-                    <i class="bi bi-envelope flex-shrink-0"></i>
-                    <div>
-                        <h3>Email Us</h3>
-                        <p>info@example.com</p>
-                    </div>
-                </div>
-
-            </div>
-        </section>
-        <!-- /Contact Section -->
     </main>
 
     <footer id="footer" class="footer dark-background">
