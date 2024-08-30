aec5db556860163ac86163706b037f09b58259b3
BPS1500 <sandy.pradana8886@gmail.com>
2024-08-30 08:17:13 +0700

    route dan mainlayout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,138 +17,61 @@ use CodeIgniter\Router\RouteCollection;
 
 
 $routes->get('/', 'Home::index');
-$routes->get('dashboard', 'Publikasi::dashboard', ['filter' => 'authGuard']);
 $routes->get('auth/login', 'Auth::login');
 $routes->get('login', 'Auth::login');
 $routes->get('auth/ssoCallback', 'Auth::ssoCallback');
 $routes->get('auth/logout', 'Auth::logout');
-$routes->get('auth/switchRole/(:num)', 'Auth::switchRole/$1');
-$routes->get('publikasi', 'Publikasi::index');
-$routes->get('Publikasi', 'Publikasi::index');
-$routes->get('Publikasi/ajupublikasi', 'Publikasi::Ajupublikasi');
-$routes->post('Publikasi/Judulpublikasi/(:num)', 'Publikasi::Judulpublikasi/$1');
-$routes->get('Publikasi/LihatKomentar/(:num)', 'Publikasi::LihatKomentar/$1');
-$routes->get('pengajuan/publikasi', 'Publikasi::index');
-$routes->get('pemeriksaan/publikasi', 'Publikasi::index');
-$routes->post('Publikasi/InsertData', 'Publikasi::InsertData');
-$routes->get('pemeriksaan/sprp', 'PemeriksaanController::index');
-$routes->get('pemeriksaan/sprp', 'PemeriksaanController::save_no_publikasi/$1');
-$routes->post('pemeriksaan/save_no_publikasi', 'PemeriksaanController::save_no_publikasi');
-$routes->get('pemeriksaan/get_details/(:num)', 'PemeriksaanController::get_details/$1');
-$routes->post('/pemeriksaan/store_nomor_publikasi', 'PemeriksaanController::store_nomor_publikasi');
-// $routes->get('pemeriksaan/pemeriksaan', 'Pemeriksaan::index');
 
-// Routes for SPRP CRUD
-$routes->get('/sprp', 'SprpController::index');
-$routes->get('pengajuan/sprp', 'SprpController::index');
-$routes->get('/sprp/create', 'SprpController::create');
-$routes->post('/sprp/store', 'SprpController::store');
-$routes->get('/sprp/edit/(:num)', 'SprpController::edit/$1');
-$routes->post('/sprp/update/(:num)', 'SprpController::update/$1');
-$routes->get('/sprp/delete/(:num)', 'SprpController::delete/$1');
-$routes->get('sprp/get_details/(:num)', 'SprpController::get_details/$1');
-$routes->get('sprp/updateNomorPublikasi/(:num)', 'SprpController::updateNomorPublikasi/$1');
-$routes->get('sprp/save_no_publikasi/(:num)', 'SprpController::save_no_publikasi/$1');
+$routes->group('', ['filter' => 'auth'], function ($routes) {
+    $routes->get('dashboard', 'Publikasi::dashboard');
 
-// Routes Master Publikasi
-$routes->get('kelola/masterpublikasi', 'MasterPublikasi::index');
-$routes->post('kelola/masterpublikasi/delete/(:num)', 'MasterPublikasi::delete/$1');
-$routes->post('kelola/masterpublikasi/edit/(:num)', 'MasterPublikasi::edit/$1');
-$routes->post('masterpublikasi/tambah', 'MasterPublikasi::tambah');
-$routes->post('masterpublikasi/unggahPublikasi', 'MasterPublikasi::unggahPublikasi');
-$routes->post('kelola/masterpublikasi/import', 'MasterPublikasi::unggahPublikasi');
+    $routes->get('auth/switchRole/(:num)', 'Auth::switchRole/$1');
+    $routes->get('publikasi', 'Publikasi::index');
+    $routes->get('Publikasi', 'Publikasi::index');
+    $routes->get('Publikasi/ajupublikasi', 'Publikasi::Ajupublikasi');
+    $routes->post('Publikasi/Judulpublikasi/(:num)', 'Publikasi::Judulpublikasi/$1');
+    $routes->get('Publikasi/LihatKomentar/(:num)', 'Publikasi::LihatKomentar/$1');
+    $routes->get('pengajuan/publikasi', 'Publikasi::index');
+    $routes->get('pemeriksaan/publikasi', 'Publikasi::index');
+    $routes->post('Publikasi/InsertData', 'Publikasi::InsertData');
+    $routes->get('pemeriksaan/sprp', 'PemeriksaanController::index');
+    $routes->get('pemeriksaan/sprp', 'PemeriksaanController::save_no_publikasi/$1');
+    $routes->post('pemeriksaan/save_no_publikasi', 'PemeriksaanController::save_no_publikasi');
+    $routes->get('pemeriksaan/get_details/(:num)', 'PemeriksaanController::get_details/$1');
+    $routes->post('/pemeriksaan/store_nomor_publikasi', 'PemeriksaanController::store_nomor_publikasi');
+    // $routes->get('pemeriksaan/pemeriksaan', 'Pemeriksaan::index');
 
-// Routes Komentar
-$routes->post('publikasi/addkomentar', 'Publikasi::AddKomentar');
+    // Routes for SPRP CRUD
+    $routes->get('/sprp', 'SprpController::index');
+    $routes->get('pengajuan/sprp', 'SprpController::index');
+    $routes->get('/sprp/create', 'SprpController::create');
+    $routes->post('/sprp/store', 'SprpController::store');
+    $routes->get('/sprp/edit/(:num)', 'SprpController::edit/$1');
+    $routes->post('/sprp/update/(:num)', 'SprpController::update/$1');
+    $routes->get('/sprp/delete/(:num)', 'SprpController::delete/$1');
+    $routes->get('sprp/get_details/(:num)', 'SprpController::get_details/$1');
+    $routes->get('sprp/updateNomorPublikasi/(:num)', 'SprpController::updateNomorPublikasi/$1');
+    $routes->get('sprp/save_no_publikasi/(:num)', 'SprpController::save_no_publikasi/$1');
 
-$routes->post('Publikasi/getStatusOptions', 'Publikasi::getStatusOptions');
-$routes->post('Publikasi/updateStatus', 'Publikasi::updateStatus');
-$routes->post('publikasi/editkomentar', 'Publikasi::editKomentar');
-$routes->post('publikasi/deletekomentar/(:num)', 'Publikasi::deleteKomentar/$1');
-$routes->post('publikasi/updateStatus', 'Publikasi::updateStatus');
-$routes->post('Publikasi/deletePublikasi/(:num)', 'Publikasi::deletePublikasi/$1');
-$routes->post('Publikasi/updateLink', 'Publikasi::updateLink');
+    // Routes Master Publikasi
+    $routes->get('kelola/masterpublikasi', 'MasterPublikasi::index');
+    $routes->post('kelola/masterpublikasi/delete/(:num)', 'MasterPublikasi::delete/$1');
+    $routes->post('kelola/masterpublikasi/edit/(:num)', 'MasterPublikasi::edit/$1');
+    $routes->post('masterpublikasi/tambah', 'MasterPublikasi::tambah');
+    $routes->post('masterpublikasi/unggahPublikasi', 'MasterPublikasi::unggahPublikasi');
+    $routes->post('kelola/masterpublikasi/import', 'MasterPublikasi::unggahPublikasi');
 
-$routes->get('Publikasi/getReplies', 'Publikasi::getReplies');
-$routes->post('publikasi/addReply', 'Publikasi::addReply');
+    // Routes Komentar
+    $routes->post('publikasi/addkomentar', 'Publikasi::AddKomentar');
 
+    $routes->post('Publikasi/getStatusOptions', 'Publikasi::getStatusOptions');
+    $routes->post('Publikasi/updateStatus', 'Publikasi::updateStatus');
+    $routes->post('publikasi/editkomentar', 'Publikasi::editKomentar');
+    $routes->post('publikasi/deletekomentar/(:num)', 'Publikasi::deleteKomentar/$1');
+    $routes->post('publikasi/updateStatus', 'Publikasi::updateStatus');
+    $routes->post('Publikasi/deletePublikasi/(:num)', 'Publikasi::deletePublikasi/$1');
+    $routes->post('Publikasi/updateLink', 'Publikasi::updateLink');
 
-
-
-
-
-
-
-
-// $routes->get('/layanan/form', 'Layanan::form');
-// $routes->get('/layanan/tabel', 'Layanan::tabel');
-// $routes->post('/layanan/submit', 'Layanan::submit');
-
-// $routes->get('/pengajuan_layanan_ti', 'LayananTI::index');
-// $routes->get('/pengajuan_layanan_ti/create', 'LayananTI::create');
-// $routes->post('/pengajuan_layanan_ti/store', 'LayananTI::store');
-// $routes->get('/pengajuan_layanan_ti/(:num)', 'LayananTI::show/$1');
-// $routes->get('/pengajuan_layanan_ti/(:num)/edit', 'LayananTI::edit/$1');
-// $routes->post('/pengajuan_layanan_ti/(:num)/update', 'LayananTI::update/$1');
-// $routes->post('/pengajuan_layanan_ti/(:num)/delete', 'LayananTI::delete/$1');
-// $routes->get('/pengajuan_layanan_ti/assign_technician/(:num)', 'LayananTI::assignTechnician/$1');
-// $routes->post('/pengajuan_layanan_ti/saveTechnicianAssignment/(:num)', 'LayananTI::saveTechnicianAssignment/$1');
-// $routes->post('/pengajuan_layanan_ti/addAction/(:num)', 'LayananTI::addAction/$1');
-// $routes->get('/pengajuan_layanan_ti/print_receipt/(:num)', 'LayananTI::printReceipt/$1');
-
-
-// $routes->group('', ['filter' => 'auth'], function ($routes) {
-//     $routes->get('/dashboard', 'Dashboard::index');
-
-    // Layanan TI
-    // $routes->group('layanan_ti', function ($routes) {
-    // $routes->get('pengajuan', 'LayananTI::pengajuan');
-    // $routes->get('status', 'LayananTI::status');
-    // $routes->get('penanganan', 'LayananTI::penanganan', ['filter' => 'teknisiOrAdmin']);
-    //     $routes->get('pengajuan', 'LayananTI::index');
-    //     $routes->get('create', 'LayananTI::create');
-    //     $routes->post('store', 'LayananTI::store');
-    //     $routes->get('assign/(:num)', 'LayananTI::assign/$1');
-    //     $routes->get('print/(:num)', 'LayananTI::print/$1');
-    //     $routes->get('status', 'StatusController::index');
-    //     $routes->get('penanganan', 'PenangananController::index', ['filter' => 'role:teknisi,admin']);
-    // });
-
-
-//     $routes->group('layanan_ti', function ($routes) {
-//         $routes->get('/', 'LayananTI::index');
-//         $routes->get('pengajuan', 'LayananTI::index');
-//         $routes->get('create', 'LayananTI::create');
-//         $routes->get('/layanan_ti/edit/(:num)', 'LayananTI::edit/$1');
-//         $routes->post('/layanan_ti/update/(:num)', 'LayananTI::update/$1');
-
-//         $routes->post('store', 'LayananTI::store');
-//         $routes->get('assign/(:num)', 'LayananTI::assign/$1');
-//         $routes->post('assign_store/(:num)', 'LayananTI::assign_store/$1');
-
-//         $routes->get('print/(:num)', 'LayananTI::print/$1');
-//         $routes->get('update_status/(:num)', 'LayananTI::updateStatus/$1');
-//         $routes->get('penanganan', 'PenangananController::index');
-//     });
-
-//     // Pengaturan (Only for Admin)
-//     $routes->group('pengaturan', ['filter' => 'admin'], function ($routes) {
-//         $routes->get('/', 'Pengaturan::index');
-//         $routes->get('create', 'Pengaturan::create');
-//         $routes->post('store', 'Pengaturan::store');
-//         $routes->get('edit/(:num)', 'Pengaturan::edit/$1');
-//         $routes->post('update/(:num)', 'Pengaturan::update/$1');
-//         $routes->get('delete/(:num)', 'Pengaturan::delete/$1');
-//     });
-
-//     // Aksi Layanan TI
-//     $routes->group('aksi_layanan_ti', function($routes) {
-//         $routes->get('/', 'AksiLayananTIController::index');
-//         $routes->get('index', 'AksiLayananTIController::index');
-//         $routes->get('create/(:num)', 'AksiLayananTIController::create/$1');
-//         $routes->post('store', 'AksiLayananTIController::store');
-//         $routes->get('log/(:num)', 'AksiLayananTIController::log/$1');
-//         $routes->get('updateStatus/(:num)/(:any)', 'AksiLayananTIController::updateStatus/$1/$2');
-//         $routes->get('printTandaTerima/(:num)', 'AksiLayananTIController::printTandaTerima/$1');
-//     });
-// });
+    $routes->get('Publikasi/getReplies', 'Publikasi::getReplies');
+    $routes->post('publikasi/addReply', 'Publikasi::addReply');
+});
--- app/Views/layouts/main_layout.php
+++ app/Views/layouts/main_layout.php
@@ -5,7 +5,7 @@
     <!-- Meta tags and other head elements -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $this->renderSection('title') ?></title>
+    <title>AAjusi BPS</title>
     <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/fontawesome-free/css/all.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/dist/css/adminlte.min.css') ?>">
@@ -24,7 +24,13 @@
     <!-- Summernote css/js -->
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 
-</head>
+    <style>
+        .sidebar-collapse .brand-link img#brand-logo {
+            display: none;
+        }
+    </style>
+
+
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -38,22 +44,14 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="<?= base_url('/') ?>" class="brand-link">
-                <img src="<?= base_url('assets/AdminLTE/dist/img/BPSAdminLTELogo.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img width="50px" src="<?= base_url('assets/img/bps.png') ?>" alt="AdminLTE Logo" class="" style="opacity: .8">
                 <span class="brand-text font-weight-light">AAJUSI BPS</span>
             </a>
 
             <!-- Sidebar -->
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="<?= base_url('assets/AdminLTE/dist/img/user2-160x160.jpg') ?>" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block"><?= session()->get('full_name') ?></a>
-                        <p class="d-block text-muted"><?= session()->get('role_name') ?></p>
-                    </div>
-                </div>
+
 
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
@@ -80,14 +78,15 @@
                         $currentURL = current_url();
 
                         // Fungsi untuk menampilkan menu
-                        function display_menu($parent_id, $menuArray, $currentURL) {
+                        function display_menu($parent_id, $menuArray, $currentURL)
+                        {
                             if (isset($menuArray[$parent_id])) {
                                 foreach ($menuArray[$parent_id] as $menu) {
                                     $active = ($currentURL == base_url($menu->menu_link)) ? 'active' : '';
                                     if (isset($menuArray[$menu->id])) {
                                         echo '<li class="nav-item has-treeview ' . $active . '">';
                                         echo '<a href="#" class="nav-link ' . $active . '">';
-                                        echo '<i class="nav-icon fas fa-list"></i>'; 
+                                        echo '<i class="nav-icon fas fa-list"></i>';
                                         echo '<p>' . $menu->menu_name . '<i class="right fas fa-angle-left"></i></p>';
                                         echo '</a>';
                                         echo '<ul class="nav nav-treeview">';
@@ -97,7 +96,7 @@
                                     } else {
                                         echo '<li class="nav-item">';
                                         echo '<a href="' . base_url($menu->menu_link) . '" class="nav-link ' . $active . '">';
-                                        echo '<i class="nav-icon fas fa-circle"></i>';
+                                        echo '<i class="nav-icon"></i>';
                                         echo '<p>' . $menu->menu_name . '</p>';
                                         echo '</a>';
                                         echo '</li>';
@@ -135,7 +134,7 @@
     <script src="<?= base_url('assets/AdminLTE/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
     <script src="<?= base_url('assets/AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') ?>"></script>
     <script src="<?= base_url('assets/AdminLTE/plugins/select2/js/select2.min.js') ?>"></script> <!-- Select2 JS -->
-    
+
     <script>
         $(document).ready(function() {
             $('.select2').select2();
@@ -212,7 +211,7 @@
             $('#summernote').summernote();
         });
     </script>
-    
+
     <script>
         $('#summernote-edit').summernote({
             height: 150,
