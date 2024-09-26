d0b237ad76a68d6ce9c7ae23221b2abbf70e0c2c
BPS1500 <sandy.pradana8886@gmail.com>
2024-09-26 13:54:07 +0700

    perbaikkan proses update link

--- app/Views/pengajuan_publikasi/index.php
+++ app/Views/pengajuan_publikasi/index.php
@@ -3,107 +3,107 @@
 <?= $this->section('content') ?>
 <div class="container-fluid mt-5">
     <div class="card shadow-sm">
-            <div class="card-header bg-primary text-white">
-                <div class="d-flex justify-content-between align-items-center">
-                    <h3 class="card-title mb-0"><?= $judul ?></h3>
-                    <a href="<?= base_url('Publikasi/ajupublikasi') ?>" class="btn btn-light btn-sm"></i> Ajukan Publikasi</a>
-                </div>
+        <div class="card-header bg-primary text-white">
+            <div class="d-flex justify-content-between align-items-center">
+                <h3 class="card-title mb-0"><?= $judul ?></h3>
+                <a href="<?= base_url('Publikasi/ajupublikasi') ?>" class="btn btn-light btn-sm"></i> Ajukan Publikasi</a>
             </div>
         </div>
-
-        <!-- /.card-header -->
-        <div class="card-body table-responsive">
-            <?php
-            if (session()->getFlashdata('insert')) {
-                echo '<div class="alert alert-success">';
-                echo session()->getFlashdata('insert');
-                echo '</div>';
-            }
-            ?>
-
-            <table id="tabel_publikasi" class="table-bordered table-hover">
-                <thead  class="table table-striped table-bordered" style="text-align: center;">
-                    <tr>
-                        <th style="width: 10px">No</th>
-                        <th>Jenis</th>
-                        <th>Judul Publikasi</th>
-                        <th style="width: 30px">Fungsi Pengusul</th>
-                        <th>Nama Penyusun</th>
-                        <th>Tanggal Pengajuan</th>
-                        <th>Tanggal Perbaikan</th>
-                        <th style="width: 170px">Tautan</th>
-                        <th>Status</th>
-                        <th style="width: 120px">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody class="table-secondary">
-                    <?php $no = 1;
-                    foreach ($Publikasi as $key => $value) { ?>
-
-                        <tr data-id="<?= $value['id_publikasi'] ?>">
-                            <td style="text-align: center;"><?= $no++  ?>.</td>
-                            <td style="text-align: center;"><?php echo ($value['id_jenispublikasi'] == 1) ? "ARC" : "NON ARC"; ?></td>
-                            <td><?= $value['judul_publikasi_ind'] ?></td>
-                            <td style="text-align: center;"><?= $value['nama_fungsi'] ?></td>
-                            <td><?= $value['nama_penyusun'] ?></td>
-                            <td><?= date('d-m-Y', strtotime($value['tgl_pengajuan'])) ?></td>
-                            <td><?= $value['tgl_repisi'] ? date('d-m-Y', strtotime($value['tgl_repisi'])) : '-' ?></td>
-                    
-                            <td>
-                                <?php if (session()->get('role') == 4): ?>
-                                    <button class="btn btn-primary update-link" data-id="<?= $value['id_publikasi'] ?>" data-type="publikasi" data-toggle="modal" data-target="#updateLinkModal"><i class="fas fa-book"></i></button>
-                                    <?php if ($value['link_spsnrkf'] != null): ?>
-                                        <button class="btn btn-secondary update-link" data-id="<?= $value['id_publikasi'] ?>" data-type="spsnrkf" data-toggle="modal" data-target="#updateLinkModal"><i class="fas fa-file-signature"></i></button>
-                                    <?php endif; ?>
-                                    <?php if ($value['link_spsnres2'] != null): ?>
-                                        <button class="btn btn-success update-link" data-id="<?= $value['id_publikasi'] ?>" data-type="spsnres2" data-toggle="modal" data-target="#updateLinkModal"><i class="fas fa-file-signature"></i></button>
-                                    <?php endif; ?>
-                                <?php else: ?>
-                                    <a class="btn btn-primary" href="<?= $value['link_publikasi'] ?>" target="_blank"><i class="fas fa-book"></i></a>
-                                    <?php if ($value['link_spsnrkf'] != null): ?>
-                                        <a class="btn btn-secondary" href="<?= $value['link_spsnrkf'] ?>" target="_blank"><i class="fas fa-file-signature"></i></a>
-                                    <?php endif; ?>
-                                    <?php if ($value['link_spsnres2'] != null): ?>
-                                        <a class="btn btn-success" href="<?= $value['link_spsnres2'] ?>" target="_blank"><i class="fas fa-file-signature"></i></a>
-                                    <?php endif; ?>
-                                <?php endif; ?>
-                            </td>
-
-                            <td class="status <?= $value['bgcolor']; ?>"><?= $value['status_review']; ?></td>
-
-                            <td style="text-align: center;" align=center>
-                                <a href="<?= base_url('Publikasi/LihatKomentar') ?>/<?= $value['id_publikasi'] ?>" class="btn btn-primary btn-sm ubah" data-data="<?= $value['id_publikasi'] ?>"
-                                   data-toggle="tooltip" data-placement="top" title="Lihat Komentar">
-                                    <i class="fas fa-comment"></i>
-                                </a>
-                                <?php if (in_array(session()->get('role'), [1, 3])): ?>
-                                    <!-- Button Update Status -->
-                                    <button type="button" class="btn btn-info btn-sm btn-status" data-id="<?= $value['id_publikasi'] ?>" data-toggle="modal" data-target="#statusModal"
-                                            data-toggle="tooltip" data-placement="top" title="Status Publikasi">
-                                        <i class="fas fa-sync-alt"></i>
-                                    </button>
-                                <?php endif; ?>
-
-                                <!-- Button Delete Komentar -->
-                                <?php 
-                                if ($value['flag'] == 5 && (session()->get('role') == 1 || session()->get('role') == 4)) { 
-                                ?>
-                                    <button type="button" class="btn btn-danger btn-sm btn-delete" data-id="<?= $value['id_publikasi'] ?>" 
-                                            data-toggle="modal" data-target="#deleteModal"
-                                            data-toggle="tooltip" data-placement="top" title="Hapus Publikasi">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                <?php 
-                                } 
-                                ?>
-                            </td>
-                        </tr>
-                    <?php  } ?>
-                </tbody>
-            </table>
-        </div>
-        </div>
     </div>
+
+    <!-- /.card-header -->
+    <div class="card-body table-responsive">
+        <?php
+        if (session()->getFlashdata('insert')) {
+            echo '<div class="alert alert-success">';
+            echo session()->getFlashdata('insert');
+            echo '</div>';
+        }
+        ?>
+
+        <table id="tabel_publikasi" class="table-bordered table-hover">
+            <thead class="table table-striped table-bordered" style="text-align: center;">
+                <tr>
+                    <th style="width: 10px">No</th>
+                    <th>Jenis</th>
+                    <th>Judul Publikasi</th>
+                    <th style="width: 30px">Fungsi Pengusul</th>
+                    <th>Nama Penyusun</th>
+                    <th>Tanggal Pengajuan</th>
+                    <th>Tanggal Perbaikan</th>
+                    <th style="width: 170px">Tautan</th>
+                    <th>Status</th>
+                    <th style="width: 120px">Aksi</th>
+                </tr>
+            </thead>
+            <tbody class="table-secondary">
+                <?php $no = 1;
+                foreach ($Publikasi as $key => $value) { ?>
+
+                    <tr data-id="<?= $value['id_publikasi'] ?>">
+                        <td style="text-align: center;"><?= $no++  ?>.</td>
+                        <td style="text-align: center;"><?php echo ($value['id_jenispublikasi'] == 1) ? "ARC" : "NON ARC"; ?></td>
+                        <td><?= $value['judul_publikasi_ind'] ?></td>
+                        <td style="text-align: center;"><?= $value['nama_fungsi'] ?></td>
+                        <td><?= $value['nama_penyusun'] ?></td>
+                        <td><?= date('d-m-Y', strtotime($value['tgl_pengajuan'])) ?></td>
+                        <td><?= $value['tgl_repisi'] ? date('d-m-Y', strtotime($value['tgl_repisi'])) : '-' ?></td>
+
+                        <td>
+                            <?php if (session()->get('role') == 4): ?>
+                                <button class="btn btn-primary update-link" data-id="<?= $value['id_publikasi'] ?>" data-type="publikasi" data-toggle="modal" data-target="#updateLinkModal"><i class="fas fa-book"></i></button>
+                                <?php if ($value['link_spsnrkf'] != null): ?>
+                                    <button class="btn btn-secondary update-link" data-id="<?= $value['id_publikasi'] ?>" data-type="spsnrkf" data-toggle="modal" data-target="#updateLinkModal"><i class="fas fa-file-signature"></i></button>
+                                <?php endif; ?>
+                                <?php if ($value['link_spsnres2'] != null): ?>
+                                    <button class="btn btn-success update-link" data-id="<?= $value['id_publikasi'] ?>" data-type="spsnres2" data-toggle="modal" data-target="#updateLinkModal"><i class="fas fa-file-signature"></i></button>
+                                <?php endif; ?>
+                            <?php else: ?>
+                                <a class="btn btn-primary" href="<?= $value['link_publikasi'] ?>" target="_blank"><i class="fas fa-book"></i></a>
+                                <?php if ($value['link_spsnrkf'] != null): ?>
+                                    <a class="btn btn-secondary" href="<?= $value['link_spsnrkf'] ?>" target="_blank"><i class="fas fa-file-signature"></i></a>
+                                <?php endif; ?>
+                                <?php if ($value['link_spsnres2'] != null): ?>
+                                    <a class="btn btn-success" href="<?= $value['link_spsnres2'] ?>" target="_blank"><i class="fas fa-file-signature"></i></a>
+                                <?php endif; ?>
+                            <?php endif; ?>
+                        </td>
+
+                        <td class="status <?= $value['bgcolor']; ?>"><?= $value['status_review']; ?></td>
+
+                        <td style="text-align: center;" align=center>
+                            <a href="<?= base_url('Publikasi/LihatKomentar') ?>/<?= $value['id_publikasi'] ?>" class="btn btn-primary btn-sm ubah" data-data="<?= $value['id_publikasi'] ?>"
+                                data-toggle="tooltip" data-placement="top" title="Lihat Komentar">
+                                <i class="fas fa-comment"></i>
+                            </a>
+                            <?php if (in_array(session()->get('role'), [1, 3])): ?>
+                                <!-- Button Update Status -->
+                                <button type="button" class="btn btn-info btn-sm btn-status" data-id="<?= $value['id_publikasi'] ?>" data-toggle="modal" data-target="#statusModal"
+                                    data-toggle="tooltip" data-placement="top" title="Status Publikasi">
+                                    <i class="fas fa-sync-alt"></i>
+                                </button>
+                            <?php endif; ?>
+
+                            <!-- Button Delete Komentar -->
+                            <?php
+                            if ($value['flag'] == 5 && (session()->get('role') == 1 || session()->get('role') == 4)) {
+                            ?>
+                                <button type="button" class="btn btn-danger btn-sm btn-delete" data-id="<?= $value['id_publikasi'] ?>"
+                                    data-toggle="modal" data-target="#deleteModal"
+                                    data-toggle="tooltip" data-placement="top" title="Hapus Publikasi">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            <?php
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                <?php  } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
 </div>
 
 <!-- Modal for updating links -->
@@ -195,7 +195,7 @@
                 url: '<?= base_url('Publikasi/deletePublikasi') ?>/' + deleteId,
                 type: 'POST',
                 success: function(response) {
-                    if(response.success) {
+                    if (response.success) {
                         location.reload();
                     } else {
                         alert('Gagal menghapus publikasi');
@@ -248,14 +248,14 @@
         // Handle form submission
         $('#statusForm').on('submit', function(e) {
             e.preventDefault();
-            
+
             $.ajax({
                 url: $(this).attr('action'),
                 method: 'POST',
                 data: $(this).serialize(),
                 dataType: 'json',
                 success: function(response) {
-                    if(response.success) {
+                    if (response.success) {
                         $('#statusModal').modal('hide');
                         location.reload();
                     } else {
@@ -307,8 +307,14 @@
                 dataType: 'json',
                 success: function(response) {
                     if (response.success) {
+
                         $('#updateLinkModal').modal('hide');
-                        location.reload();
+                        // location.reload();
+                        Swal.fire({
+                            title: "Berhasil!",
+                            text: "Tautan sudah diperbarui",
+                            icon: "success"
+                        });
                     } else {
                         alert('Gagal memperbarui tautan');
                     }
